d25d8465767f30a1fcdfb8abf9c7dca27c67b63a
Augusto <augustoxx2015@gmail.com>
2023-12-08 00:20:54 -0300

    codigo fronted y base

--- MyPortfolioV2/MyPortfolioV2/settings.py
+++ MyPortfolioV2/MyPortfolioV2/settings.py
@@ -64,7 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'MyportfolioV2.context_processors.project_context',
+                'MyPortfolioV2.context_processors.project_context',
             ],
         },
     },
